04b8d34f9695e60741e2fd88aea74af47474ba4c
Andrey Vetrov <vetrik@me.com>
2023-01-28 12:19:36 +0300

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Андрей Ветров on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
